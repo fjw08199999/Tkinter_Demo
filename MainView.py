ed196038dc732fc955ded0fcdee18a98aeccaf23
Fred.Fu <fjw08199999@gmail.com>
2022-05-16 12:39:33 +0800

    Initial commit

---
+++ MainView.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+import time
+
+# 設定root視窗參數
+rootWindows = tk.Tk()
+rootWindows.geometry("600x300")
+labelDemo = tk.Label
+
+for i in range(1, 10):
+    ans = 1 * i
+    aaa = ("1" + "x" + str(i) + "=" + str(ans))
+    print(type(aaa), aaa)
+
+
+name: str = "bob"
+
+clock = time.strftime("%Y-%m-%d %H:%M:%S")
+
+print(type(clock))
+
+print("今天我要和" + name + "去吃飯, 我們約晚上" + clock + "點")
+
+# Label放置
+labelDemo = tk.Label(rootWindows, text=aaa)
+labelDemo.pack()
+
+
+rootWindows.mainloop()
